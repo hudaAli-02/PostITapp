9206d5cf6f168f26612ee5ada3432bc36b7b009e
hudaAli-02 <36S2017@utas.edu.om>
2024-12-16 12:45:37 +0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 
 
 //Database connection
-const connectString ="mongodb+srv://admin:admin@postitcluster.llvss.mongodb.net/postITDb?retryWrites=true&w=majority&appName=PostITCluster";
+const connectString =`mongodb+srv://${ENV.DB_USER}:${ENV.DB_PASSWORD}@${ENV.DB_CLUSTER}/${ENV.DB_NAME}?retryWrites=true&w=majority&appName=PostITCluster";
 
 mongoose.connect(connectString, {
     useNewUrlParser: true,
