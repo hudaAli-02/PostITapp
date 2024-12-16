256a0980411629c632fdd64f25ef20ae83b92c10
hudaAli-02 <36S2017@utas.edu.om>
2024-12-16 13:18:09 +0400

    Update User.js

--- client/src/Features/User.js
+++ client/src/Features/User.js
@@ -2,6 +2,7 @@ import { createAsyncThunk,createSlice } from "@reduxjs/toolkit";
 import { UserData } from "../ExampleData";
 //import { act } from "react";
 import axios from "axios";
+import * as ENV from "../config";
 //const initialState = {value: UserData};
 
 
